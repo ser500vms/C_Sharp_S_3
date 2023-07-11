d126d7998a89a967cce3c8c84a983f40c14bdbdd
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-11 14:45:48 +0300

    Done t2 and t3

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+
+void ShowRange(int quater)
+{
+    if (quater == 1) Console.WriteLine ("Диапазон будет : x > 0 & y > 0"); 
+    if (quater == 2) Console.WriteLine ("Диапазон будет : x < 0 & y > 0"); 
+    if (quater == 3) Console.WriteLine ("Диапазон будет : x < 0 & y < 0"); 
+    if (quater == 4) Console.WriteLine ("Диапазон будет : x > 0 & y < 0"); 
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int quater = GetInput("Введите номер четверти: ");
+ShowRange(quater);
